19ba0eaccd757e3edc2623643942084ee39c6ac0
ln-dev7 <leonelngoya@gmail.com>
2022-10-26 01:43:06 +0100

    v-bind

---
+++ dyma-vue/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 :class="classtitle" :[idtitle]="classtitle">Bonjour monde</h1>
+</template>
+
+<script setup lang="ts">
+  const classtitle = 'title'
+  const idtitle = 'id'
+</script>
+
+<style scoped>
+
+</style>
